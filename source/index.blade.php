0b7a1dbc2e541cde151599f69b3c647c9e6bf087
ozzyalegre <125777377+ozzyalegre@users.noreply.github.com>
2023-12-06 14:01:32 -0500

    new logo

--- source/index.blade.php
+++ source/index.blade.php
@@ -5,12 +5,13 @@
     <div class="flex h-screen items-center justify-center relative isolate px-6 lg:px-8">
       <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56 ">
         <div class="text-center">
-          <h1 class="text-6xl font-bold tracking-tight text-white sm:text-6xl">GeoDesign</h1>
-          <p class="mt-6 text-xl leading-8 text-white">Building Beautiful, Progressive, and Eco-friendly Spaces.</p>
+          <img class="block w-2/3 mx-auto" src="/img/GeoDesignW.png" alt="GeoDesign">
+          {{-- <h1 class="text-6xl font-bold tracking-tight text-white sm:text-6xl">GeoDesign</h1>
+          <p class="mt-6 text-xl leading-8 text-white">Building Beautiful, Progressive, and Eco-friendly Spaces.</p> --}}
         </div>
       </div>
     </div>
-    <footer class="text-white absolute pl-10 mb-32 bottom-0 z-100">
+    <footer class="text-white absolute pl-10 mb-24 bottom-0 z-100">
         <a href="#" class="block">305.555.5555
         </a>
         <a href="#" class="block">george@geodesignmiami.com</i>
